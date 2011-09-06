b7188271c4dd2a2b18f974f9ed5968336cc9a341
Daniel Enman <enmand@unerror.com>
2011-09-06 15:11:20 -0300

    Adding base SVN C++ module
    This forms the basis of the NodeJS module for Subversion (the C++
    portion). This will allow interactions via. subversion.js to a
    subversion repository or working copy.

--- src/svn.cc
+++ src/svn.cc
@@ -1,8 +1,58 @@
 #include "svn.h"
-#include "svnfs.h"
 
-class SVN : ObjectWrap
+void SVN::Initiaize(Handle<Object> target)
 {
-protected:
-	char * repo_path;
-};
+	HandleScope scope;
+
+	Local<FunctionTemplate> t = FunctionTemplate::New(New);
+
+	t->InstanceTemplate()->SetInternalFieldCount(1);
+	t->SetClassName(String::NewSymbol("SVN"));
+
+	NODE_SET_PROTOTYPE_METHOD(t, "open", Open);
+
+	t->PrototypeTemplate()->SetAccessor(String::NewSymbol("path"), PathGetter);
+
+	target->Set(String::NewSymbol("SVN"), t->GetFunction());
+}
+
+Handle<Value> SVN::New(const Arguments &args)
+{
+	HandleScope scope;
+
+	if(args.Length() == 0 || !args[0]->IsString())
+	{
+		return ThrowException(String::New("Must provide repository path as an arguement"));
+	}
+
+	String::Utf8Value path(args[0]->ToString());
+	SVN *svn = new SVN(*path);
+	svn->Wrap(args.This());
+
+	return args.This();
+}
+
+Handle<Value> SVN::PathGetter(Local<String> property, const AccessorInfo& info)
+{
+	SVN *svn = ObjectWrap::Unwrap<SVN>(info.This());
+	assert(svn);
+
+	HandleScope scope;
+
+	return scope.Close(String::NewSymbol("DerpKyleHerp"));
+}
+
+Handle<Value> SVN::Open(const Arguments &args)
+{
+	SVN *svn = ObjectWrap::Unwrap<SVN>(args.This());
+	assert(svn);
+
+	return Null();
+}
+
+extern "C" void
+init (Handle<Object> target)
+{
+	HandleScope scope;
+	SVN::Initiaize(target);
+}
--- src/svn.h
+++ src/svn.h
@@ -13,11 +13,50 @@
 #ifndef SVN_H
 #define SVN_H
 
+// Node/V8
 #include <node.h>
+#include <node_events.h>
 #include <v8.h>
+
+// SVN Includes
+#include <svn_repos.h>
+#include <svn_ra.h>
 #include <svn_fs.h>
+#include <svn_pools.h>
+#include <svn_path.h>
+#include <svn_utf.h>
+
+// Other
+#include <stdlib.h>
+
+#define PATH String::NewSymbol("path");
 
 using namespace node;
 using namespace v8;
+
+class SVN : ObjectWrap
+{
+public:
+	SVN(const char *path) : path(path) {
+	}
+
+	~SVN() {
+	}
+	static void Initiaize(Handle<Object> target);
+protected:
+	static Handle<Value> New(const Arguments &args);
+
+	// Class methods
+	static Handle<Value> Open(const Arguments &args);
+	static Handle<Value> Read(const Arguments &args);
+
+	// Accessors
+	static Handle<Value> PathGetter(Local<String> property, const AccessorInfo& info);
+
+	// Inceptors
+private:
+	const char *path;
+	apr_pool_t *pool;
+};
 
 #endif
