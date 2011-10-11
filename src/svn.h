cfb2dbf575272cde0e12586d5740c4fbc2e921cd
Daniel Enman <enmand@unerror.com>
2011-10-11 14:59:42 -0300

    Adding additional SVN functions for reference
    While these functions may be removed eventually in-favour of a more
    apparent code flow, the basic SVN commands have been added to the header
    file to-be implemented.
    
    This gives us a good idea on what we need to implement for this library
    to work properly.

--- src/svn.h
+++ src/svn.h
@@ -22,6 +22,7 @@
 #include <svn_repos.h>
 #include <svn_ra.h>
 #include <svn_fs.h>
+#include <svn_wc.h>
 #include <svn_pools.h>
 #include <svn_path.h>
 #include <svn_utf.h>
@@ -29,22 +30,40 @@
 // Other
 #include <stdlib.h>
 
-#define PATH String::NewSymbol("path");
-
 using namespace node;
 using namespace v8;
 
 class SVN : ObjectWrap
 {
 public:
-	SVN(const char *path) : path(path) {
-	}
+	SVN(const char *path) : path(path) {}
+	static void Initiaize(Handle<Object> target); // V8/Node initializer
 
-	~SVN() {
-	}
-	static void Initiaize(Handle<Object> target);
+	static Handle<Value> checkout(const Arguments &args);
+	static Handle<Value> import(const Arguments &argss);
+	static Handle<Value> blame(const Arguments &args);
+	static Handle<Value> cat(const Arguments &args);
+	static Handle<Value> copy(const Arguments &args);
+	static Handle<Value> del(const Arguments &args);
+	static Handle<Value> exp(const Arguments &args);
+	static Handle<Value> info(const Arguments &args);
+	static Handle<Value> list(const Arguments &args);
+	static Handle<Value> log(const Arguments &args);
+	static Handle<Value> mkdir(const Arguments &args);
+	static Handle<Value> move(const Arguments &args);
+	static Handle<Value> add(const Arguments &args);
+	static Handle<Value> cleanup(const Arguments &args);
+	static Handle<Value> commit(const Arguments &args);
+	static Handle<Value> revert(const Arguments &args);
+	static Handle<Value> status(const Arguments &args);
+	static Handle<Value> update(const Arguments &args);
+
+	// Filesystem functions (svn_fs)
+	
+
+	~SVN() {}
 protected:
-	static Handle<Value> New(const Arguments &args);
+	static Handle<Value> New(const Arguments &args); // 'new' construct
 
 	// Class methods
 	static Handle<Value> Open(const Arguments &args);
